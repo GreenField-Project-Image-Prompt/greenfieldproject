e7b8938ef8157677160b8830aad9820d24ca906b
Emre Guler <mfbg1985@gmail.com>
2023-07-07 04:17:54 +0300

    required added

--- greenfield_server/controllers/imgController.js
+++ greenfield_server/controllers/imgController.js
@@ -16,7 +16,7 @@ const postOneImg = async (req, res) => {
     const newImg = await Img.create(req.body);
     res.json({ msg: "Img successfully uploaded" });
   } catch (err) {
-    res.send(err);
+    res.status(409).json({ msg: err.msg });
   }
 };
 //DELETE A IMG
@@ -57,65 +57,3 @@ module.exports = {
   updateImg,
   getAllUserImg,
 };
-
-// const img = require("../modules/todo");
-
-// const getAllImg = async (req, res) => {
-//   try {
-//     const img = await Img.find({});
-//     res.json(img);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-// //POST*************
-
-// const postOneImg = async (req, res) => {
-//   try {
-//     const newImg = await post.create(req.body);
-//     newImg.save();
-//     res.status(201).json({ msg: "img uploaded" });
-//   } catch (error) {
-//     res.status(409).json({ msg: error.msg });
-//   }
-// };
-// // app.post("/upload", async (req, res) => {
-// //   try {
-// //     const newImg = await post.create(req.body);
-// //     newImg.save();
-// //     res.status(201).json({ msg: "img uploaded" });
-// //   } catch (error) {
-// //     res.status(409).json({ msg: error.msg  });
-// //   }
-// // });
-
-// // app.post("/", upload.single("image"), (req, res, next) => {
-// //     const data = {image: req.file.path,};
-// //     cloudinary.uploader.upload(data.image).then((result) => {
-// //         const image = new imgModel({
-// //             img: result.url,
-// //         });
-// //         const response = image.save();
-// //         res.status(200).send({
-// //             message: "success",
-// //             result,
-// //         });
-// //     })
-// //     .catch((error) => {
-// //         res.status(500).send({
-// //             message: "failure",
-// //             error,
-// //         });
-// //     });
-// // });
-
-// // const postOneTodo = async (req, res) => {
-// //     try {
-// //       const newTodo = await Todo.create(req.body);
-// //       //res.json(newTodo);
-// //       res.json({ msg: "success" });
-// //     } catch (err) {
-// //       res.send(err);
-// //     }
-// //   };
-// module.exports = { getAllImg, postOneImg };
--- greenfield_server/modules/img.js
+++ greenfield_server/modules/img.js
@@ -3,7 +3,7 @@ const connection = require("./connection");
 
 const schema = new mongoose.Schema(
   {
-    Base64Img: { type: String, required: true },
+    Base64Img: { type: String, required: true,unique: true },
     prompt: { type: String, required: true },
     userId: String,
   },
