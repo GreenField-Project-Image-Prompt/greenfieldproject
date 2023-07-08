80f28f5a5c67808298174168fb15f2a01727c162
DPapachristou <d.k.papachristou@gmail.com>
2023-07-08 20:38:56 +0300

    ImageUpload Page

--- greenfield_Client/src/App.jsx
+++ greenfield_Client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Profile from "./components/Profile";
+import ImageUpload from "./ImageUpload";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={<Login />}></Route>
             <Route path="/signup" element={<Signup />}></Route>
             <Route path="/profile" element={<Profile />}></Route>
+            <Route path="/imageUpload" element={<ImageUpload/>}></Route>
           </Routes>
         </div>
       </Router>
---
+++ greenfield_Client/src/ImageUpload.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react";
+
+function ImageUpload(){
+    const [image,setImage] = useState('')
+    //input handler
+    function HandleImage(e) {
+        console.log(e.target.file)
+        setImage(e.target.files[0])
+    }
+    //Connection with backend
+    function HandleApi(){
+        const formData = new FormData()
+        formData.append("image",image)
+        axios.post("url", formData).then((res) => {
+            console.log(res)
+        })
+    }
+    return (
+        <div>
+            <input type="file" name="file" onChange={HandleImage}/>
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default ImageUpload;
